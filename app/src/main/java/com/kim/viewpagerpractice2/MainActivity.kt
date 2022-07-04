25a83c7633a7118c17a74507c657fa7dfe9cc0c6
KJH <daeding7@naver.com>
2022-07-04 12:52:34 +0900

    프래그먼트 약식 완성

--- app/src/main/java/com/kim/viewpagerpractice2/MainActivity.kt
+++ app/src/main/java/com/kim/viewpagerpractice2/MainActivity.kt
@@ -2,10 +2,20 @@ package com.kim.viewpagerpractice2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kim.viewpagerpractice2.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mViewPagerAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+        viewPager.adapter = mViewPagerAdapter
+
+        viewPager.offscreenPageLimit = 5
     }
 }
